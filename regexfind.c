4b0a050d53b3b5f7f581f8eac1f6427ba355b1ec
Niko Rosvall <niko@byteptr.com>
2017-03-04 19:35:29 +0200

    Add regex
    Signed-off-by: Niko Rosvall <niko@byteptr.com>

---
+++ regexfind.c
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 Niko Rosvall <niko@byteptr.com>
+ */
+
+#define _XOPEN_SOURCE 700
+/* Make only POSIX.2 regexp functions available */
+#define _POSIX_C_SOURCE 200112L
+
+#include <stdio.h>
+#include <regex.h>
+#include "regexfind.h"
+
+void regex_find(const char *search)
+{
+    regex_t regex;
+
+    if(regcomp(&regex, search, REG_ICASE) != 0)
+    {
+        fprintf(stderr, "Invalid regular expression.\n");
+    }
+
+
+}
---
+++ regexfind.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2017 Niko Rosvall <niko@byteptr.com>
+ */
+
+ #ifndef __REGEXFIND_H
+ #define __REGEXFIND_H
+
+void regex_find(const char *search);
+
+ #endif
